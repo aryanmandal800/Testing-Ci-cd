48c64f9a01768e6e02916dae9c4fd9f6e3639712
aryan-weiteredge <aryan@weiteredge.com>
2026-01-06 16:37:44 +0530

    testing chal rha hai

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ async function startServer() {
       console.log(`🚀 Server is running on port ${PORT}`);
     });
   } catch (error) {
-    console.error('❌ Unable to connect to the database:', error);
+    console.error('❌ Unable to connect to  the database:', error);
     process.exit(1);
   }
 }
